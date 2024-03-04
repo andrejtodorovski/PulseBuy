16dd1dcbee223e1b19c7b9c35bd92729ba71aeb3
Andrej Todorovski <andrejtodorovski5@gmail.com>
2024-03-04 23:23:34 +0100

    small changes

--- frontend/src/lib/footer.svelte
+++ frontend/src/lib/footer.svelte
@@ -3,63 +3,47 @@
   import "../styles/global.css";
 </script>
 
-<div class="footer">
+<footer class="text-center text-white p-4">
   <div class="container pb-3">
-    <section>
-      <footer class="text-center text-white">
-        <div class="container p-5 pb-1">
-          <div class="row d-flex justify-content-center">
-            <div class="col-md-4">
-              <img
-                src="https://i.ibb.co/1sYbSM7/image-2024-03-04-224316654.png"
-                class="logo"
-                alt="logo"
-                width="250"
-              />
-            </div>
-            <div class="col-md-4">
-              <p class="d-flex justify-content-center align-items-center">
-                <a href="/"> About us </a>
-              </p>
-              <p class="d-flex justify-content-center align-items-center">
-                <a href="/">Contact</a>
-              </p>
-            </div>
-            <div class="col-md-4">
-              {#if isUserLoggedIn()}
-                <p class="d-flex justify-content-center align-items-center">
-                  <span class="me-3">View my profile</span>
-                  <a href="/profile" class="btn btn-outline-light">
-                    My profile
-                  </a>
-                </p>
-                <p class="d-flex justify-content-center align-items-center">
-                  <span class="me-3">Log out of your account</span>
-                  <button class="btn btn-warning" on:click={removeAuthToken}
-                    >Logout</button
-                  >
-                </p>
-              {:else}
-                <p class="d-flex justify-content-center align-items-center">
-                  <span class="me-3">Register for free</span>
-                  <a href="/register" class="btn btn-warning"> Sign up! </a>
-                </p>
-                <p class="d-flex justify-content-center align-items-center">
-                  <span class="me-3">Already have an account?</span>
-                  <a href="/login" class="btn btn-outline-light"> Sign in! </a>
-                </p>
-              {/if}
-            </div>
-          </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-4 pt-2">
+        <img
+          src="https://i.ibb.co/1sYbSM7/image-2024-03-04-224316654.png"
+          class="logo"
+          alt="logo"
+          width="250"
+        />
+      </div>
+      <div class="col-md-4 pt-2">
+        <p class="d-flex justify-content-center align-items-center">
+          <a href="/"> About us </a>
+        </p>
+        <p class="d-flex justify-content-center align-items-center">
+          <a href="/">Contact</a>
+        </p>
+      </div>
+      <div class="col-md-4 pt-2">
+        <div
+          class="d-flex flex-column justify-content-center align-items-center"
+        >
+          <h5>Subscribe to our Newsletter</h5>
+          <form class="d-flex">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              class="form-control me-2"
+            />
+            <button type="submit" class="btn btn-light">Subscribe</button>
+          </form>
         </div>
-        <div class="text-center p-1">© 2024 Copyright: PulseBuy</div>
-      </footer>
-    </section>
+      </div>
+    </div>
   </div>
-</div>
+  <div class="text-center p-1">© 2024 Copyright: PulseBuy</div>
+</footer>
 
 <style>
-  .footer {
+  footer {
     background-color: #232327;
   }
   a {
