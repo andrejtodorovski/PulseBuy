5524be2d093355355fe109793fc1183528767f58
Andrej Todorovski <andrejtodorovski5@gmail.com>
2024-06-15 17:19:57 +0200

    minor fixes

--- frontend/src/lib/chat-button.svelte
+++ frontend/src/lib/chat-button.svelte
@@ -3,7 +3,7 @@
     import MessagesRepository from "../repository/messagesRepository";
     import { toasts } from "svelte-toasts";
     import { SendMessageDto } from "../models/message";
-    import { getSessionCookieValue, getUserId } from "../helpers/helpers";
+    import { getSessionCookieValue, getUserId, isUserAdmin } from "../helpers/helpers";
 
     let showChat = false;
     let content = '';
@@ -146,12 +146,14 @@
         flex-direction: column;
     }
 </style>
-<span class='chat-button' on:click={toggleChat}>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat"
-         viewBox="0 0 16 16">
-  <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
-</svg>
-</span>
+{#if !isUserAdmin()}
+    <span class='chat-button' on:click={toggleChat}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat"
+             viewBox="0 0 16 16">
+      <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
+    </svg>
+    </span>
+{/if}
 {#if showChat}
     <div class="card chat-interface">
         <div class="card-header d-flex align-items-center justify-content-between">
@@ -166,9 +168,9 @@
         <div class="card-body chat-body">
             {#each messages as msg}
                 {#if msg.isAdminReply}
-                <div class="message support">
-                    {msg.content}
-                </div>
+                    <div class="message support">
+                        {msg.content}
+                    </div>
                 {:else}
                     <div class="message customer">
                         {msg.content}
--- frontend/src/lib/nav.svelte
+++ frontend/src/lib/nav.svelte
@@ -137,12 +137,12 @@
               d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
           </svg>
         </a>
-
+        {/if}
         <a href="" class="cursor-pointer" on:click={toggleNotifications} style="position: relative;">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill"
                viewBox="0 0 16 16">
             <path
-              d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
+                    d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2m.995-14.901a1 1 0 1 0-1.99 0A5 5 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901" />
           </svg>
           {#if unreadCount > 0}
             <span class="notification-badge">{unreadCount}</span>
@@ -164,7 +164,6 @@
             </div>
           {/if}
         </a>
-        {/if}
         <a on:click={removeAuthToken} href="/">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                class="bi bi-box-arrow-right" viewBox="0 0 16 16">
